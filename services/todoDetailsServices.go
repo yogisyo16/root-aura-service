e8529d817c26d10ad421084777455ec6720a67e2
yogis <yogisyo16@gmail.com>
2025-12-29 03:53:57 +0700

    Update: TodoDetails prepare

--- services/todoDetailsServices.go
+++ services/todoDetailsServices.go
@@ -11,13 +11,12 @@ import (
 )
 
 type TodoDetails struct {
-	todoId        string    `json:"todo_id,omitempty" bson:"_todo_id,omitempty"`
-	todo          Todo      `json:"todo,omitempty" bson:"_todo,omitempty"`
-	user          User      `json:"user,omitempty" bson:"_user,omitempty"`
-	taskDetails   string    `json:"task_details,omitempty" bson:"_task_details,omitempty"`
-	collaborators []User    `json:"collaborators,omitempty" bson:"_collaborators,omitempty"`
-	createdAt     time.Time `json:"created_at,omitempty" bson:"_created_at,omitempty"`
-	updatedAt     time.Time `json:"update_at,omitempty" bson:"_update_at,omitempty"`
+	ID          string    `json:"id,omitempty" bson:"_id,omitempty"`
+	TodoID      string    `json:"todo_id,omitempty" bson:"_todo_id,omitempty"`
+	TaskDetails string    `json:"task_details,omitempty" bson:"_task_details,omitempty"`
+	Notes       string    `json:"notes,omitempty" bson:"_notes,omitempty"`
+	CreatedAt   time.Time `json:"created_at,omitempty" bson:"_created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty" bson:"_updated_at,omitempty"`
 }
 
 func NewTodoDetailsService(mongo *mongo.Client) TodoDetails {
@@ -25,12 +24,17 @@ func NewTodoDetailsService(mongo *mongo.Client) TodoDetails {
 	return TodoDetails{}
 }
 
-func (t *TodoDetails) getAllTodosDetails() ([]TodoDetails, error) {
-	collection := returnTodosCollection("todos")
+func returnTodoDetailsCollection(collection string) *mongo.Collection {
+	return client.Database("todos_db").Collection(collection)
+}
+
+// GetAllTodosDetails - now exported
+func (t *TodoDetails) GetAllTodosDetails() ([]TodoDetails, error) {
+	collection := returnTodoDetailsCollection("todo_details")
 	var todoDetails []TodoDetails
 	cursor, err := collection.Find(context.TODO(), bson.D{})
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return nil, err
 	}
 
@@ -38,15 +42,19 @@ func (t *TodoDetails) getAllTodosDetails() ([]TodoDetails, error) {
 
 	for cursor.Next(context.Background()) {
 		var todoDetail TodoDetails
-		cursor.Decode(&todoDetail)
+		if err := cursor.Decode(&todoDetail); err != nil {
+			log.Println(err)
+			continue
+		}
 		todoDetails = append(todoDetails, todoDetail)
 	}
 
 	return todoDetails, nil
 }
 
-func (t *TodoDetails) getTodoDetailsById(id string) (TodoDetails, error) {
-	collection := returnTodosCollection("todos")
+// GetTodoDetailsById - now exported
+func (t *TodoDetails) GetTodoDetailsById(id string) (TodoDetails, error) {
+	collection := returnTodoDetailsCollection("todo_details")
 	var todoDetail TodoDetails
 
 	mongoID, err := primitive.ObjectIDFromHex(id)
@@ -61,4 +69,71 @@ func (t *TodoDetails) getTodoDetailsById(id string) (TodoDetails, error) {
 	}
 
 	return todoDetail, nil
+}
+
+// InsertTodoDetails - create todo details
+func (t *TodoDetails) InsertTodoDetails(entry TodoDetails) error {
+	collection := returnTodoDetailsCollection("todo_details")
+	_, err := collection.InsertOne(context.TODO(), TodoDetails{
+		TodoID:      entry.TodoID,
+		TaskDetails: entry.TaskDetails,
+		Notes:       entry.Notes,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	})
+
+	if err != nil {
+		log.Println("Error: ", err)
+		return err
+	}
+	return nil
+}
+
+// UpdateTodoDetails - update todo details
+func (t *TodoDetails) UpdateTodoDetails(id string, entry TodoDetails) (*mongo.UpdateResult, error) {
+	collection := returnTodoDetailsCollection("todo_details")
+	mongoID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	update := bson.D{
+		{Key: "$set", Value: bson.D{
+			{Key: "_task_details", Value: entry.TaskDetails},
+			{Key: "_notes", Value: entry.Notes},
+			{Key: "_updated_at", Value: time.Now()},
+		}},
+	}
+
+	res, err := collection.UpdateOne(
+		context.Background(),
+		bson.M{"_id": mongoID},
+		update,
+	)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return res, nil
+}
+
+// DeleteTodoDetails
+func (t *TodoDetails) DeleteTodoDetails(id string) error {
+	collection := returnTodoDetailsCollection("todo_details")
+	mongoID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	_, err = collection.DeleteOne(
+		context.Background(),
+		bson.M{"_id": mongoID},
+	)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
 }
